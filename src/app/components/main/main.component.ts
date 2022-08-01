81890bff000756d0344d7db7b500c01a4918d3da
mhashirmaroof <hashirbhatti35@gmail.com>
2022-08-01 17:28:35 +0500

    Catagory Model UI

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-
+  showCatagorynameModel = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openModelCatagory(){
+    this.showCatagorynameModel = !this.showCatagorynameModel;
   }
 
 }
