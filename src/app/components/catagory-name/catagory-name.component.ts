672b99eb156fe69eb23ebaaf290272cdfba380b9
Muhammad Hashir Maroof <hashirbhatti35@gmail.com>
2022-08-01 15:11:13 +0500

    Header added, bootstrap added 4.6.0

---
+++ src/app/components/catagory-name/catagory-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catagory-name',
+  templateUrl: './catagory-name.component.html',
+  styleUrls: ['./catagory-name.component.scss']
+})
+export class CatagoryNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
